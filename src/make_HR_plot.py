844ddfc2d141964695e949135c7eab64793fa50b
Avi Vajpeyi <avi.vajpeyi@gmail.com>
2022-09-08 12:10:02 +1000

    update lims

--- src/make_HR_plot.py
+++ src/make_HR_plot.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+# %load_ext jupyternotify
+
 # +
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -43,6 +45,28 @@ def add_1914_annotations(ax):
     y2 = m1 * x2 + c1[0] * -0.3
     ax.plot(x1,y1, ls='dashed', color='gray', zorder=-100)
     ax.plot(x2,y2, ls='dashed', color='gray', zorder=-100)
+
+    
+def add_hip_annotations(ax):
+    df = pd.read_csv("data/hyg_hr_theory_data.csv")
+    groups = list(set([i.split("_")[0] for i in df.columns.values]))
+    for i, g in enumerate(groups[::-1]):
+        x, y = df[f"{g}_X"], df[f"{g}_Y"]
+        if 'dwarfs' in g:
+            l = 20
+            x, y = x[0:l], y[0:l]
+        f = interp1d(x, y)
+
+        xnew = np.arange(min(x), max(x), 0.01)
+        ynew = gaussian_filter1d(f(xnew), 15, mode="nearest")
+        ax.plot(xnew, ynew, alpha=0.7, ls='dashed', c=f"C{i}")
+    fs = 13
+    ax.annotate("Giants", xy=(0.1, -1), xycoords="data", fontsize=fs,rotation=-40.)
+    ax.annotate("Subgiants", xy=(1.1,3.5), xycoords="data", fontsize=fs, )
+    ax.annotate("Main Sequence", xy=(0, 9.), xycoords="data", fontsize=fs, rotation=-40)
+    ax.annotate("White Dwarfs", xy=(-0.1, 14.5), xycoords="data", fontsize=fs, rotation=-40)
+
+
 
 
 
@@ -92,88 +116,28 @@ def load_1914_data():
     
 
 def load_hyg_data():
-    data = pd.read_csv("https://raw.githubusercontent.com/astronexus/HYG-Database/master/hygdata_v3.csv")
+    try:
+        data = pd.read_csv("data/hyg_data.csv")
+    except Exception:
+        data = pd.read_csv("https://raw.githubusercontent.com/astronexus/HYG-Database/master/hygdata_v3.csv")
     data['B-V'] = data['ci']
     data['temp'] = bv_to_temp(data['B-V'])
     data['mg'] = data['absmag']
     data = data[['mg','temp', 'ci']]
     data = data.dropna()
     return data
-
-
-
     
 def load_hip_data():
-    """
-    To download the catalog you must access the VizieR Search Page 
-    http://vizier.u-strasbg.fr/viz-bin/VizieR-3?-source=I/239/hip_main
-    for the catalog I/239 from the Strasbourg astronomical Data Center (CDS).
-    Select only the fields listed above: HIP, Vmag, Plx, BV, SpType. 
-    In the left pane, under "Preferences" select "unlimited" and "; -Separated Values" 
-    Press the "Submit" button and download the file, which I called I_239_selection.tsv 
-    and occupies only 3.8 MB. Opening it with a text editor is found that it is a text file 
-    with a header of a few lines explanatory of the contents, followed by 118322 entries with 
-    the five selected fields separated by the ; character. At the end of the file there is 
-    also a blank line we should skip when reading the file.
-    
-    
-    """
-    HIP = "data/hip.tsv"
-    df = pd.read_table(HIP, skiprows=44, sep='|', header=None, index_col=0,
-                   names = ['HIP', 'Vmag', 'Plx', 'B-V', 'SpType'],
-                   skipfooter=1, engine='python')
-    
-    df = df.applymap(lambda x: np.nan if isinstance(x, str)
-                           and x.isspace() else x).dropna()
-
-    df['Vmag'] = df['Vmag'].astype(np.float64)
-    df['Plx'] = df['Plx'].astype(np.float64)
-    df['B-V'] = df['B-V'].astype(np.float64)
-    
-    # removing nans in parallaxes and B-V
-    df = df[ df['Plx'].notna()]
-    df = df[df["B-V"].notna()]
-    
-    #Making sure parallaxes are positive and not too close to zero
-    df = df[df.Plx > 0.1] 
-    df["d_kpc"] = 1/df["Plx"]
-    #     df["d_kpc"].describe()
-    
-
-    #creating a copy of the SpTypes without changes to the Spectral Types for later use 
-    df['Og_SpType'] = df['SpType']
-
-    #the first 2 identifiers are the most important information in the Spectral types, so we drop all other letters in the column
-    df['SpType'] = df['SpType'].str[:2]
-
-    #we also remove spectral types that are < 2 characters, we need a type and a number 
-    mask = (df['SpType'].str.len() > 1)
-    df = df.loc[mask]
-
-    #we remove spectral types where the second character is not useable, ex: b:, h', mm
-    mask = df['SpType'].str[1].str.isdigit()
-    df = df.loc[mask]
-
-    #we are only looking at the most common spectral types, O, B A, F, G, K, M, so any other spectral types will be removed 
-    df = df[df['SpType'].map(lambda s: s[0] in 'OBAFGKM')]
-
-    #the x axis of the HR-diagram will be the spectral type, so we quantify this by assigning a number to each spectral type
-    sequence = {'O':'0', 'B':'1', 'A':'2', 'F':'3', 'G':'4', 'K':'5', 'M':'6'}
-    df['SpType'] = df['SpType'].apply(lambda s: sequence[s[0]]+s[1])
-
-    #convert string to float
-    df['SpType'].apply(lambda s: float(s))
-    
-    
-    # Add a new column with the absolute magnitude
-    df['mg'] = mag_to_absmag(df['Vmag'], df['Plx'])
-    df['temp'] = bv_to_temp(df['B-V'])
-   
-    df = df.replace([np.inf, -np.inf], np.nan)
-    df = df.dropna()
-    
-    df['ci'] = df['B-V']
-    return df[['mg','temp', 'ci']]
+    try:
+        data = pd.read_csv("data/hyg_data.csv")
+    except Exception:
+        data = pd.read_csv("https://raw.githubusercontent.com/astronexus/HYG-Database/master/hygdata_v3.csv")
+    data['B-V'] = data['ci']
+    data['temp'] = bv_to_temp(data['B-V'])
+    data['mg'] = data['absmag']
+    data = data[['mg','temp', 'ci', 'hip']]
+    data = data.dropna()
+    return data
 
 
     
@@ -232,20 +196,37 @@ def bv_to_temp(bv):
 
 
 
+DATA = { "1914":load_1914_data(), 'hyg': load_hyg_data(), 'hip':load_hip_data(), 'gaia':load_gaia_data()}
+
 # -
 
-data_1914 = load_1914_data()
-data_gaia = load_gaia_data()
-data_hyg = load_hyg_data()
+def plot_hr(datasets=list(DATA.keys()), fname="hr_diagram.png"):
+    num = len(datasets)
+    data = {k:DATA[k] for k in datasets}
+    fig, axes = plt.subplots(1,num,  figsize=(4*num, 5), sharey=True)
+    
+    for (k, d), ax in zip(data.items(), axes):
+        if k == "1914":
+            make_hr_diagram(ax, d,alpha=1,  l="a)",  diff_xargs=True, show_ylab=True)
+            add_1914_annotations(ax)
+        elif k == "hip":
+            make_hr_diagram(ax, d, l="b)", diff_xargs=True, alpha=0.1)
+            add_hip_annotations(ax)
+        elif k == "hyg":
+            make_hr_diagram(ax, d, l="bi)", diff_xargs=True, alpha=0.1)
+            add_hyg_annotations(ax)
+        elif k == "gaia":
+            make_hr_diagram(ax, d, l="c)", show_colorbar=True)
+    
+    
+    if 'hyg' not in datasets:  
+        for ax in axes:
+            ax.set_ylim(17.5, -5)
+    plt.tight_layout()
+    plt.savefig(fname, dpi=300)
 
-# +
-fig, axes = plt.subplots(1,3,  figsize=(12, 5), sharey=True)
-make_hr_diagram(axes[0], data_1914,alpha=1,  l="a)",  diff_xargs=True, show_ylab=True)
-make_hr_diagram(axes[1], data_hyg, l="b)", diff_xargs=True, alpha=0.1)
-make_hr_diagram(axes[2], data_gaia, l="c)", show_colorbar=True)
+# %%notify
+plot_hr(datasets=['1914','hip', 'gaia'], fname="hr_diagram.png")
 
-add_1914_annotations(axes[0])
-add_hyg_annotations(axes[1])
-
-plt.tight_layout()
-plt.savefig("hr_diagram.png", dpi=300)
+# %%notify
+plot_hr(datasets=['1914','hip', 'hyg', 'gaia'], fname="hr_diagram_all.png")
